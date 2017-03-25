81b3336564fcd1eb5bfb0d48cc23f3d14711be38
KarlaRobinson <karla@conekta.io>
2017-03-25 16:26:19 -0600

    include mongodb module

--- server.js
+++ server.js
@@ -5,28 +5,36 @@ const Hapi = require('hapi');
 const server = new Hapi.Server();
 server.connection({ port: 3000, host: 'localhost' });
 
-server.route({
-    method: 'GET',
-    path: '/',
-    handler: function (request, reply) {
-        reply('Hello, world!');
-    }
-});
-
-server.route({
-    method: 'GET',
-    path: '/{name}',
-    handler: function (request, reply) {
-        reply('Hello, ' + encodeURIComponent(request.params.name) + '!');
-    }
-});
-
-server.start((err) => {
+server.register(require('inert'), (err) => {
 
     if (err) {
         throw err;
     }
-    console.log(`Server running at: ${server.info.uri}`);
+
+    server.route({
+        method: 'GET',
+        path: '/',
+        handler: function (request, reply) {
+            reply.file('./book/index.html');
+        }
+    });
+
+});
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'POST',
+        path: '/show',
+        handler: function (request, reply) {
+            reply('Searching');
+        }
+    });
+
 });
 
 server.register(require('inert'), (err) => {
@@ -37,9 +45,34 @@ server.register(require('inert'), (err) => {
 
     server.route({
         method: 'GET',
-        path: '/hello',
+        path: '/show',
         handler: function (request, reply) {
-            reply.file('./public/hello.html');
+            reply.file('./book/show.html');
         }
     });
+
+});
+
+server.register(require('inert'), (err) => {
+
+    if (err) {
+        throw err;
+    }
+
+    server.route({
+        method: 'POST',
+        path: '/books',
+        handler: function (request, reply) {
+            reply.file('./book/index.html');
+        }
+    });
+
+});
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
 });
